50858d89818bd40d737d9f9c77d237b299eaeb29
sdkurjnk <sdkurjnk@naver.com>
2025-06-25 19:24:11 +0900

    6th commit

--- Table.py
+++ Table.py
@@ -6,6 +6,7 @@ column = 4
 data = [['0' for i in range(column)] for j in range(row)]
 command = ["adrow", "adcol", "set", "rm", "save", "exit"]
 
+
 #Making Empey Data Table
 data[0][0] = "MyFile"
 for i in range(1, row):
@@ -71,6 +72,14 @@ def main(stdscr):
             display_win.addstr(output + "\n")
         display_win.refresh()
 
+    def printerror(errorcode):
+        if errorcode == 1001:
+            status_win.addstr("<Error> : Argument Error")
+            status_win.refresh()
+        elif errorcode == 1002:
+            status_win.addstr("<Error> : Index Error")
+            status_win.refresh()
+
     #Working Part
     while True:
         input_win.clear()
@@ -93,16 +102,14 @@ def main(stdscr):
         else: #Command Process
             if (inputcommand[0] == "adrow"): #"adrow" Cammand
                 if (len(inputcommand) != 1):
-                    status_win.addstr("<Error> : Argument Error")
-                    status_win.refresh()
+                    printerror(1001)
                 else:
                     data.append(['0' for _ in range(column)])
                     row += 1
                     data[len(data)-1][0] = f"row{row-1}"
             elif (inputcommand[0] == "adcol"): #"adcolumn" Command
                 if (len(inputcommand) != 1):
-                    status_win.addstr("<Error> : Argument Error")
-                    status_win.refresh()
+                    printerror(1001)
                 else:
                     for r in data:
                         r.append('0')
@@ -110,34 +117,28 @@ def main(stdscr):
                     data[0][column-1] = f"col{column-1}"
             elif (inputcommand[0] == "set"): #"setdata" Command
                 if (len(inputcommand) != 4):
-                    status_win.addstr("<Error> : Argument Error")
-                    status_win.refresh()
+                    printerror(1001)
                 elif ((int(inputcommand[1]) > row) or (int(inputcommand[2]) > column)):
-                    status_win.addstr("<Error> : Index error")
-                    status_win.refresh()
+                    printerror(1002)
                 else:
                     data[int(inputcommand[1])][int(inputcommand[2])] = inputcommand[3]
             elif (inputcommand[0] == "rm"): #"rmdata" Command
                 if (len(inputcommand) != 3):
-                    status_win.addstr("<Error> : Argument Error")
-                    status_win.refresh()
+                    printerror(1001)
                 elif ((int(inputcommand[1]) > row) or (int(inputcommand[2]) > column)):
-                    status_win.addstr("<Error> : Index error")
-                    status_win.refresh()
+                    printerror(1002)
                 else:
                     data[int(inputcommand[1])][int(inputcommand[2])] = '0'
             elif (inputcommand[0] == "save"): #"save" Command
                 if (len(inputcommand) != 1):
-                    status_win.addstr("<Error> : Argument Error")
-                    status_win.refresh()
+                    printerror(1001)
                 else:
                     Saving()
                     status_win.addstr(f"File was saved in {data[0][0]}.txt")
                     status_win.refresh()
             elif (inputcommand[0] == "exit"): #"exit" Command
                 if (len(inputcommand) > 1):
-                    status_win.addstr("<Error> : Too many argument")
-                    status_win.refresh()
+                    printerror(1002)
                 else:
                     break
 
